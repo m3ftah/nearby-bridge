72c6840a93285baa9252758a310fb97c7f2cc17b
Lakhdar Meftah <guemar93@gmail.com>
2018-03-19 19:03:44 +0100

    changed impl

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
 var constants = require('./constants');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'public');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
 
 var discEndpointSockets = [];
 
@@ -17,14 +34,7 @@ var acceptingConnection = [];
 
 var connected = [];
 
-var port = process.env.PORT || 3000;
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
-
-// Routing
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Chatroom
 
